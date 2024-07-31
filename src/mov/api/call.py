8e5b85e32e5628fd78fa11821052135f3faea220
sunwoo <summerham22@gmail.com>
2024-07-31 17:34:54 +0900

    url

--- src/mov/api/call.py
+++ src/mov/api/call.py
@@ -5,23 +5,20 @@ import pandas as pd
 def echo(yaho):
     return yaho
 
-def req(load_dt="20120101", url_param={}, url_nation={}):
-    url = gen_url(load_dt, url_param, url_nation)
+def req(load_dt="20120101", url_param={}):
+    url = gen_url(load_dt, url_param)
     r = requests.get(url)
     data = r.json()
     code = r.status_code
     print(data)
     return code, data
 
-def gen_url(dt="20120101", url_param={"multiMovieYn":"N"}, url_nation={"repNationCd": "K"}):
+def gen_url(dt="20120101", url_param={}):
     base_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
     key = get_key()
     url = f"{base_url}?key={key}&targetDt={dt}"
     for key, value in url_param.items():
         url = url + f"&{key}={value}"
-
-    for k, v in url_nation.items():
-        url = url + f"&{k}={v}"
 
     return url
 
@@ -30,23 +27,23 @@ def get_key():
     key = os.getenv('MOVIE_API_KEY')
     return key
 
-def req2list(load_dt='20120101', url_param={}, url_nation={}):
-    _, data = req(load_dt, url_param, url_nation)
+def req2list(load_dt='20120101', url_param={}):
+    _, data = req(load_dt, url_param)
     l = data['boxOfficeResult']['dailyBoxOfficeList']
 
     return l
 
-def list2df(load_dt='20120101', url_param={}, url_nation={}):
-    l = req2list(load_dt, url_param, url_nation)
+def list2df(load_dt='20120101', url_param={}):
+    l = req2list(load_dt, url_param)
     df = pd.DataFrame(l)
 
     return df
 
-def save2df(load_dt='20120101', url_param={}, url_nation={}):
-    df = list2df(load_dt, url_param, url_nation)
+def save2df(load_dt='20120101', url_param={}):
+    df = list2df(load_dt, url_param)
     df['load_dt'] = load_dt
     #아래 파일 저장시 load_df 기준으로 파티셔닝
-    df.to_parquet('~/tmp/test_parquet/load_dt', partition_cols=['load_dt'])
+    #df.to_parquet('~/tmp/test_parquet/load_dt', partition_cols=['load_dt'])
     
     return df
 
